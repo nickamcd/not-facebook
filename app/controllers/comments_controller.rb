fbea0d4756a5e6dbff671175f71507de9cb9c2d3
Nick McDaniel <nickamcd@gmail.com>
2021-04-14 15:39:07 -0700

    Comment form set up underneath posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,24 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    current_user.comments.create(post_id: params[:post_id])
-    redirect_to posts_path
+    @comment = current_user.comments.build(comment_params)
+    if comment.save
+      redirect_to posts_path
+    else
+      flash[:notice] = "Something went wrong."
+      redirect_to posts_path
+    end
   end
 
   def destroy
     @comment = current_user.likes.find(params[:id])
     @comment.destroy
     redirect_to posts_path
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:user_id, :post_id, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :posts do
     resources :likes, only: [:create, :destroy]
-    resources :comments
+    resources :comments, only: [:create, :destroy]
   end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
