01945bfafbfb842e0a5bb7d0ecf1f4aa0c4c6cbf
Nick McDaniel <nickamcd@gmail.com>
2021-04-16 10:24:05 -0700

    Very basic user index page and profile page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
   end
   devise_for :users
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "posts#index" 
