3e5a24e526aac2079ef1574dc41ce84218b4e2c1
Nick McDaniel <nickamcd@gmail.com>
2021-04-18 10:41:28 -0700

    Migration for friend and friend requests

--- db/migrate/20210418171416_create_friendships.rb
+++ db/migrate/20210418171416_create_friendships.rb
@@ -1,6 +1,7 @@
 class CreateFriendships < ActiveRecord::Migration[6.1]
   def change
     create_table :friendships do |t|
+      t.integer :friend_id
 
       t.timestamps
     end
--- db/migrate/20210418172033_create_friend_requests.rb
+++ db/migrate/20210418172033_create_friend_requests.rb
@@ -1,6 +1,8 @@
 class CreateFriendRequests < ActiveRecord::Migration[6.1]
   def change
     create_table :friend_requests do |t|
+      t.integer :sender_id
+      t.integer :receiver_id
 
       t.timestamps
     end
