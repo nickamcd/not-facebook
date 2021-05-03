914afe65a607207cdfb78bcf42b45cf2338a0cdb
Nick McDaniel <nickamcd@gmail.com>
2021-05-03 10:08:05 -0700

    Added welcome email to user sign up

--- Gemfile
+++ Gemfile
@@ -54,6 +54,8 @@ group :development do
   gem 'listen', '~> 3.3'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  # Use letter_opener for testing the mailer
+  gem 'letter_opener', '~> 1.7'
 end
 
 group :test do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  after_create :send_welcome_email
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -37,8 +39,11 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     name_split = auth.info.name.split(" ")
     user = User.where(email: auth.info.email).first
-    user ||= User.create!(provider: auth.provider, uid: auth.uid, last_name: name_split[0], first_name: name_split[1], email: auth.info.email, password: Devise.friendly_token[0, 20])
+    user ||= User.create!(provider: auth.provider, uid: auth.uid, last_name: name_split[1], first_name: name_split[0], email: auth.info.email, password: Devise.friendly_token[0, 20])
       user
   end
 
+  def send_welcome_email
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
